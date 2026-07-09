9c076ec1973e1c3b594d6012a6ea332e491494fe
GendessonSousa <gendessonsousa078@gmail.com>
2026-07-08 23:46:48 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/dev/Gendesson/Study/assistant/StudyAssistantApplication.java
@@ -0,0 +1,13 @@
+package dev.Gendesson.Study.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Gendesson/Study/assistant/StudyAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Gendesson.Study.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
